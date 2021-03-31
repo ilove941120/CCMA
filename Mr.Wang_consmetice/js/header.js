330de3e727f5bc6ce666b0751778c6496b2f50d8
ilove941120 <71624974+ilove941120@users.noreply.github.com>
2021-03-31 12:36:54 +0800

    123

--- Mr.Wang_consmetice/js/header.js
+++ Mr.Wang_consmetice/js/header.js
@@ -1,5 +1,6 @@
+// navbarRWD
 $('.navbarListItemGroup').hide();
-$(".navbarList").click(function(){
+$(".humburger").click(function(){
     $('.navbarListItemGroup').show().addClass("show");
 });
 
@@ -12,3 +13,71 @@ $(window).resize(function(){
     $('.navbarListItemGroup').hide()
 }
 });
+
+
+
+
+// 側邊購物車滑出
+$(".shoppingCartIcon").click(function(){
+    $(".SideShoppingCartBlock").fadeIn()
+    $(".SideShoppingCartBlock").animate({
+        right:"0px",
+        transition:"1s"
+    })
+})
+$(".SideShoppingBlockClose").click(function(){
+    $(".SideShoppingCartBlock").animate({
+        right:"-100%",
+        transition:"1s"
+    })
+    $(".SideShoppingCartBlock").fadeOut()
+})
+var headerLastTotal = 0;
+$(".SideShoppingCareItemTotal span").each(function(){
+        headerLastTotal += parseInt($(this).text())
+    })
+    $(".headerLastTotal span").text(headerLastTotal)
+// 增加按鈕
+$(".headerAdd").click(function(){
+    var headerLastTotal = 0;
+    var itemTotalPrice =0;
+    var itemPrice = $(this).parents(".SideShoppingCartItem").find('.SideShoppingCareItemPrice span').text()
+    var itemQuantity =$(this).siblings((".headerquantity")).text()
+    $(this).parents(".SideShoppingCartItem").find(".headerquantity").text(parseInt(itemQuantity) + 1)
+    var itemQuantityNew =$(this).parents(".SideShoppingCartItem").find(".headerquantity").text()
+    itemTotalPrice = parseInt(itemQuantityNew) * parseInt(itemPrice)
+    $(this).parents(".SideShoppingCartItem").find(".SideShoppingCareItemTotal span").text(itemTotalPrice)
+
+    $(".SideShoppingCareItemTotal span").each(function(){
+        headerLastTotal += parseInt($(this).text())
+    })
+    $(".headerLastTotal span").text(headerLastTotal)
+})
+
+// 減少按鈕
+$(".headerLess").click(function(){
+    var headerLastTotal = 0;
+    var itemTotalPrice =0;
+    var itemPrice = $(this).parents(".SideShoppingCartItem").find('.SideShoppingCareItemPrice span').text()
+    var itemQuantity =$(this).siblings((".headerquantity")).text()
+    if(itemQuantity > 0){
+    $(this).parents(".SideShoppingCartItem").find(".headerquantity").text(parseInt(itemQuantity) - 1)
+    var itemQuantityNew =$(this).parents(".SideShoppingCartItem").find(".headerquantity").text()
+    itemTotalPrice = parseInt(itemQuantityNew) * parseInt(itemPrice)
+    $(this).parents(".SideShoppingCartItem").find(".SideShoppingCareItemTotal span").text(itemTotalPrice)
+    }
+
+    $(".SideShoppingCareItemTotal span").each(function(){
+        headerLastTotal += parseInt($(this).text())
+    })
+    $(".headerLastTotal span").text(headerLastTotal)
+})
+// 刪除按鈕
+$(".SideShoppingCareItemDelete").click(function(){
+    var headerLastTotal = 0;
+    $(this).parents(".SideShoppingCartItem").remove()
+    $(".SideShoppingCareItemTotal span").each(function(){
+        headerLastTotal += parseInt($(this).text())
+    })
+    $(".headerLastTotal span").text(headerLastTotal)
+})
